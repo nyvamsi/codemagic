556986a5fcfd90e76d521e32e2c5754b36f29416
Adam Zimmerman <zimmy125@yahoo.com>
2022-10-11 22:19:37 -0500

    Added Marker Logging

--- src/app/mapv2/mapv2.page.ts
+++ src/app/mapv2/mapv2.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MapInfoWindow, MapMarker, GoogleMap } from '@angular/google-maps';
 
 @Component({
   selector: 'app-mapv2',
@@ -6,18 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['mapv2.page.scss']
 })
 export class Mapv2Page implements OnInit {
+  @ViewChild(GoogleMap, { static: false }) map: GoogleMap;
+  @ViewChild(MapInfoWindow, { static: false }) info: MapInfoWindow;
 
   constructor() {}
-  zoom = 12;
+  zoom = 17;
   center: google.maps.LatLngLiteral;
   options: google.maps.MapOptions = {
     mapTypeId: 'hybrid',
     zoomControl: false,
     scrollwheel: false,
     disableDoubleClickZoom: true,
-    maxZoom: 15,
+    maxZoom: 20,
     minZoom: 8,
   };
+
+  markers = [];
+  infoContent = '';
 
   ngOnInit() {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -34,6 +40,40 @@ export class Mapv2Page implements OnInit {
 
   zoomOut() {
     if (this.zoom > this.options.minZoom) this.zoom--;
+  }
+  
+  click(event: google.maps.KmlMouseEvent) {
+    console.log(event);
+  }
+
+  logCenter() {
+    console.log(JSON.stringify(this.map.getCenter()));
+  }
+
+  addMarker() {
+    let lat = this.center.lat + ((Math.random() - 0.5) * 2) / 10;
+    let lng = this.center.lng + ((Math.random() - 0.5) * 2) / 10;
+    this.markers.push({
+      position: {
+        lat: lat,
+        lng: lng,
+      },
+      label: {
+        color: 'red',
+        text: 'Marker label ' + (this.markers.length + 1),
+      },
+      title: 'Marker title ' + (this.markers.length + 1),
+      info: 'Marker info ' + (this.markers.length + 1),
+      options: {
+        animation: google.maps.Animation.BOUNCE,
+      },
+    });
+    console.log("Put Marker: lat: " + lat + "lng: " + lng);
+  }
+
+  openInfo(marker: MapMarker, content) {
+    this.infoContent = content;
+    this.info.open(marker);
   }
 
 }
